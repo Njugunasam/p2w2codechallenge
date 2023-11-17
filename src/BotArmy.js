61e8b983f84e820486207a6bb91ec6134c6828e7
Njugunasam <sammynjuguna5826@gmail.com>
2023-11-17 14:59:36 +0300

    defining release functionality

--- src/BotArmy.js
+++ src/BotArmy.js
@@ -1,16 +1,18 @@
 import React from 'react';
 
-const BotArmy = ({ bots }) => {
-    return (
-        <div>
-            <h2>Bot Army</h2>
-            <ul>
-                {bots.map(bot => (
-                    <li key={bot.id}>{bot.name}</li>
-                ))}
-            </ul>
-        </div>
-    );
+const BotArmy = ({ enlistedBots, releaseBot }) => {
+  return (
+    <div>
+      <h2>Your Bot Army</h2>
+      <ul>
+        {enlistedBots.map(bot => (
+          <li key={bot.id}>
+            {bot.name} <button onClick={() => releaseBot(bot)}>Release</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default BotArmy;
